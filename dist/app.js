72ea9e1c5fb3f318968236f64ffa655c831e033b
jamesschein <james@schein.co.uk>
2022-11-20 12:38:23 +0000

    Links

--- dist/app.js
+++ dist/app.js
@@ -3,15 +3,18 @@ document.addEventListener("DOMContentLoaded", () => {
   const mainMenu = document.querySelector(".main-menu");
   const mainLinks = document.querySelectorAll(".main-menu a");
 
+  // remove menu when activated on desktop view and screen is resized
   window.addEventListener("resize", () => {
     mainMenu.classList.add("hide");
   });
 
   hamburger.addEventListener("click", (e) => {
     mainMenu.classList.toggle("hide");
+    // enlarge the hamburger trigger area behind the menu so it can be clicked away
     e.target.classList.toggle("menu-active");
   });
 
+  // menu links close the menu parent after they have been triggered
   mainLinks.forEach((mainLink) => {
     mainLink.addEventListener("click", () => {
       if (mainMenu.classList.contains("hide") != -1) {
