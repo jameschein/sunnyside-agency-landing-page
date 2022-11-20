efe66481794e9a3165733cc522b214b548165e32
jamesschein <james@schein.co.uk>
2022-11-20 12:07:05 +0000

    Final sans Animated Menu

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger-control");
+  const mainMenu = document.querySelector(".main-menu");
+  const mainLinks = document.querySelectorAll(".main-menu a");
+
+  hamburger.addEventListener("click", (e) => {
+    mainMenu.classList.toggle("hide");
+    e.target.classList.toggle("menu-active");
+  });
+
+  mainLinks.forEach((mainLink) => {
+    mainLink.addEventListener("click", () => {
+      mainMenu.classList.toggle("hide");
+    });
+  });
+});
